96d58c78b24d408d4e0b9888956ef189827e47ad
Sakshi Agrawal <sagrawal@MBPl24MJPQK62G6J.local>
2025-01-11 13:53:26 +0530

    Database URL Update

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 # load_dotenv()
 
 # Get Database URL directly from environment variable
-DATABASE_URL = "postgresql://postgres:112233445566778899qwe@db.syuyrduryrlvlkrnmyzp.supabase.co:5432/postgres"
+DATABASE_URL = "postgresql://postgres.syuyrduryrlvlkrnmyzp:112233445566778899qwe@aws-0-ap-south-1.pooler.supabase.com:6543/postgres"
 
 # Additional database configuration
 CONNECT_ARGS = {} 
