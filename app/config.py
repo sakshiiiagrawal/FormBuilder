52a9a2f97a79cdb7e3ecfff90062135fe4dde060
Sakshi Agrawal <sagrawal@MBPl24MJPQK62G6J.local>
2025-01-11 13:34:42 +0530

    Update database url

--- app/config.py
+++ app/config.py
@@ -2,10 +2,10 @@ import os
 from dotenv import load_dotenv
 
 # Load environment variables
-load_dotenv()
+# load_dotenv()
 
 # Get Database URL directly from environment variable
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "postgresql://postgres:112233445566778899qwe@db.syuyrduryrlvlkrnmyzp.supabase.co:5432/postgres"
 
 # Additional database configuration
 CONNECT_ARGS = {} 
